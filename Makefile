1f9d1e75849f1a008505584cd9a3b4b04544de70
Vitalii Mikhailov <personal@aragas.org>
2025-03-20 03:26:21 +0200

    Test

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@
 # Compatible with Ubuntu 14.04.1 and macOS
 
 OBJS = cimgui.o
-# OBJS += imgui_markdown.o
+OBJS += cimmarkdown.o
 OBJS += ./imgui/imgui.o
 OBJS += ./imgui/imgui_draw.o
 OBJS += ./imgui/imgui_demo.o
--- imgui_markdown.cpp
+++
@@ -1,7 +0,0 @@
-#include "cimgui.h"
-#include "imgui_markdown/imgui_markdown.h"
-
-CIMGUI_API void igMarkdown(const char* markdown_, size_t markdownLength_, const MarkdownConfig& mdConfig_ )
-{
-    return ImGui::Markdown(markdown_, markdownLength_, mdConfig_);
-}
